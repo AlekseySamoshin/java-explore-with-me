74b320473d74f8df327e90cc355cbae9494b1413
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-04-23 11:42:08 +0400

    fix: исправление сборки проекта

--- ewm-service/src/main/java/ru/practicum/EwmMainService.java
+++ ewm-service/src/main/java/ru/practicum/EwmMainService.java
@@ -3,9 +3,9 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class EwmMainService {
     public static void main(String[] args) {
-        SpringApplication.run(EwmMainService.class, args);
+//        SpringApplication.run(EwmMainService.class, args);
     }
 }
