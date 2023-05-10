441807beccbeed826e06a94b79e85f388b472e50
Aleksey Samoshin <samoshin.aleksey@yandex.ru>
2023-05-10 09:51:08 +0300

    fix: исправление после ревью

--- ewm-service/src/main/java/ru/practicum/repository/CategoryRepository.java
+++ ewm-service/src/main/java/ru/practicum/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.model.Category;
 
 import java.util.List;
